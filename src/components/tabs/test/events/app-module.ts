f7b9beb5b07f71aa9ac1206f91333c65e840a777
mhartington <mikehartington@gmail.com>
2017-01-06 12:24:26 -0500

    test(tabs): fix test

--- src/components/tabs/test/events/app-module.ts
+++ src/components/tabs/test/events/app-module.ts
@@ -1,67 +1,5 @@
 import { Component, NgModule } from '@angular/core';
-import { Config, IonicApp, IonicModule, App, AlertController, ModalController, ViewController, Tab, Tabs, Events } from '../../../..';
-
-//
-// Modal
-//
-@Component({
-  template: `
-  <ion-header>
-    <ion-toolbar>
-      <ion-buttons start>
-        <button ion-button (click)="dismiss()">Cancel</button>
-      </ion-buttons>
-
-      <ion-title>
-        Filter Sessions
-      </ion-title>
-
-      <ion-buttons end>
-        <button ion-button (click)="dismiss()">Done</button>
-      </ion-buttons>
-    </ion-toolbar>
-  </ion-header>
-
-  <ion-content class="outer-content">
-    <ion-list>
-      <ion-list-header>Tracks</ion-list-header>
-
-      <ion-item *ngFor="let i of items">
-        <ion-label>Toggle {{i}}</ion-label>
-        <ion-toggle color="secondary"></ion-toggle>
-      </ion-item>
-    </ion-list>
-
-    <ion-list>
-      <button ion-item color="danger" detail-none>
-        Reset All Filters
-      </button>
-      <button ion-item color="danger" detail-none (click)="appNavPop()">
-        App Nav Pop
-      </button>
-    </ion-list>
-  </ion-content>
-  `
-})
-export class MyModal {
-  items: any[] = [];
-
-  constructor(private viewCtrl: ViewController, private app: App) {
-    for (var i = 1; i <= 10; i++) {
-      this.items.push(i);
-    }
-  }
-
-  dismiss() {
-    // using the injected ViewController this page
-    // can "dismiss" itself and pass back data
-    this.viewCtrl.dismiss();
-  }
-
-  appNavPop() {
-    this.app.navPop();
-  }
-}
+import { IonicApp, IonicModule, Events } from '../../../..';
 
 //
 // Tab 1
@@ -144,7 +82,6 @@ export class E2EApp {
 @NgModule({
   declarations: [
     E2EApp,
-    MyModal,
     Tab1,
     Tab2,
     TabsPage
@@ -157,7 +94,6 @@ export class E2EApp {
   bootstrap: [IonicApp],
   entryComponents: [
     E2EApp,
-    MyModal,
     Tab1,
     Tab2,
     TabsPage
